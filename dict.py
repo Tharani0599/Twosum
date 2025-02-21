78c2aa67f04ac205b78128b1af8e3473a0a0622b
Tharani Sri Rajendran <Tharani.Rajendran@frost.com>
2025-02-21 12:17:48 +0530

    dict

---
+++ dict.py
@@ -0,0 +1,12 @@
+my_cat={'play':'ball','b':'catch','color':'red'}
+print(my_cat.keys())
+print(my_cat.items())
+print(my_cat.values())
+print('\n')
+
+#checking key or value exists in dict
+
+print('catch' in my_cat)
+print('b' in my_cat.values())
+print('b' not in my_cat.values())
+
